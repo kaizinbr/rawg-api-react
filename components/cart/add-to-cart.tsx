a574a7b1099a662de8c2b648844625e3ec47a240
Kaizin <kaiolucas1812@gmail.com>
2023-12-25 23:16:36 -0300

    ajustes

--- components/cart/add-to-cart.tsx
+++ components/cart/add-to-cart.tsx
@@ -1,77 +1,9 @@
 "use client";
 
-import { PlusIcon } from "@heroicons/react/24/outline";
-import clsx from "clsx";
-import { addGame } from "@/components/cart/actions";
-import { create } from "./newActions";
-import LoadingDots from "@/components/loading-dots";
-import { ProductVariant } from "@/lib/shopify/types";
-import { useSearchParams } from "next/navigation";
-import { useFormState, useFormStatus } from "react-dom";
 import { useEffect, useReducer } from "react";
 
-function SubmitButton({ gameId }: { gameId: string | undefined }) {
-    const { pending } = useFormStatus();
-    const buttonClasses =
-        "relative flex w-full items-center justify-center rounded-full bg-blue-600 p-4 tracking-wide text-white";
-    const disabledClasses = "cursor-not-allowed opacity-60 hover:opacity-60";
-
-    // if (!availableForSale) {
-    //     return (
-    //         <button
-    //             aria-disabled
-    //             className={clsx(buttonClasses, disabledClasses)}
-    //         >
-    //             Out Of Stock
-    //         </button>
-    //     );
-    // }
-
-    // if (!selectedVariantId) {
-    //     return (
-    //         <button
-    //             aria-label="Please select an option"
-    //             aria-disabled
-    //             className={clsx(buttonClasses, disabledClasses)}
-    //         >
-    //             <div className="absolute left-0 ml-4">
-    //                 <PlusIcon className="h-5" />
-    //             </div>
-    //             Add To Cart
-    //         </button>
-    //     );
-    // }
-
-    return (
-        <button
-            onClick={(e: React.FormEvent<HTMLButtonElement>) => {
-                if (pending) e.preventDefault();
-            }}
-            aria-label="Add to cart"
-            aria-disabled={pending}
-            className={clsx(buttonClasses, {
-                "hover:opacity-90": true,
-                disabledClasses: pending,
-            })}
-        >
-            <div className="absolute left-0 ml-4">
-                {pending ? (
-                    <LoadingDots className="mb-3 bg-white" />
-                ) : (
-                    <PlusIcon className="h-5" />
-                )}
-            </div>
-            Add To Cart
-        </button>
-    );
-}
-
 export function AddToCartBtn({ gameId, gameName, gameSlug }: { gameId: string, gameName: string, gameSlug: string }) {
-    const [message, formAction] = useFormState(create, null);
-    const searchParams = useSearchParams();
-
-    // const selectedVariantId = variant?.id || defaultVariantId;
-    const actionWithVariant = formAction.bind(null, gameId);
+    
 
     function getStoredCartItems() {
         if (typeof window !== "undefined") {
@@ -146,12 +78,7 @@ export function AddToCartBtn({ gameId, gameName, gameSlug }: { gameId: string, g
 }
 
 export function AddToCartBtn2({ gameId, gameName, gameSlug }: { gameId: string, gameName: string, gameSlug: string }) {
-    const [message, formAction] = useFormState(create, null);
-    const searchParams = useSearchParams();
-
-    // const selectedVariantId = variant?.id || defaultVariantId;
-    const actionWithVariant = formAction.bind(null, gameId);
-
+    
     function getStoredCartItems() {
         if (typeof window !== "undefined") {
             const storedCartItems = localStorage.getItem("cartItems");
